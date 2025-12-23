27976be86d74c4d3c80ca27099b36b3e299f3e9b
satya devops <satyadevops143@gmail.com>
2025-12-24 04:31:53 +0530

    robo

---
+++ data.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "joindevops" {
+    owners           = ["573389908821"]
+    most_recent      = true
+    
+    filter {
+        name   = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
